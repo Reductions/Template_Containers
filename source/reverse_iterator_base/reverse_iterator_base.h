4a1989f92506a9703eccf297ed4e7c70420962bd
Reductions <slavi.boyanov@gmail.com>
2016-06-21 02:42:59 +0300

    Setup reverse iterator
    Added files for code and tests for reverse_iterator_base class

---
+++ source/reverse_iterator_base/reverse_iterator_base.h
@@ -0,0 +1,14 @@
+#ifndef _REVERSE_ITERATOR_BASE_H_
+#define _REVERSE_ITERATOR_BASE_H_
+
+namespace tc {
+
+  template <class T>
+  class reverse_iterator_base {
+  };
+
+  #include "reverse_iterator_base_code.h"
+
+}
+
+#endif//_REVERSE_ITERATOR_BASE_H_ 
---
+++ test/reverse_iterator_base/reverse_iterator_base_test.cpp
@@ -0,0 +1,2 @@
+#include "../../source/reverse_iterator_base/reverse_iterator_base.h" 
+#include "../catch.hpp"
