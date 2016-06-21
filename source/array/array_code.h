af4cf443b6661aefeb76cdbe7b110d789bcbb616
Reductions <slavi.boyanov@gmail.com>
2016-06-21 17:23:16 +0300

    Array constructors
    Added the array constructors and test for them.

--- source/array/array_code.h
+++ source/array/array_code.h
@@ -1,4 +1,21 @@
 #ifndef _ARRAY_CODE_H_
 #define _ARRAY_CODE_H_
 
+template <class T,size_t S>
+array<T,S>::array(){}
+
+template <class T,size_t S>
+array<T,S>::array(std::initializer_list<T> list){
+  if(list.size() > S)
+    throw std::invalid_argument("the list provided is too big");
+  auto it = list.begin();
+  for (int i = 0; i < S; ++i){
+    if (it != list.end())
+      _content[i] = *(++it);
+    else
+      _content[i] = T();
+  }
+}
+
+
 #endif//_ARRAY_CODE_H_
--- test/array/array_test.cpp
+++ test/array/array_test.cpp
@@ -1,2 +1,56 @@
 #include "../../source/array/array.h"
 #include "../catch.hpp"
+#include <array>
+
+SCENARIO("array can be constructed",
+	 "[construction][array]"){
+
+  GIVEN("size greater then 0"){
+
+    WHEN("default constructed"){
+      THEN("it constructs properly"){
+	REQUIRE_NOTHROW( (tc::array<int,2>()) );
+      }
+    }
+
+    WHEN("constucted with initializer list smaller then the array size"){
+      THEN("it constructs properly"){
+	REQUIRE_NOTHROW( (tc::array<int,2>{1}) );
+      }
+    }
+
+    WHEN("constucted with initializer list with the same size as the array"){
+      THEN("it constructs properly"){
+	REQUIRE_NOTHROW( (tc::array<int,2>{1,2}) );
+      }
+    }
+
+    WHEN("constucted with initializer list bigger then the array size"){
+      THEN("it throws invalid_argument exception"){
+	REQUIRE_THROWS_AS( (tc::array<int,2>{1,2,3}) , std::invalid_argument );
+      }
+    }
+  }
+
+  GIVEN("size equal to 0"){
+
+    WHEN("default constructed"){
+      THEN("it constructs properly"){
+	REQUIRE_NOTHROW( (tc::array<int,0>()) );
+      }
+    }
+
+    WHEN("constucted with empty initializer list"){
+      THEN("it constructs properly"){
+	REQUIRE_NOTHROW( (tc::array<int,0>{}) );
+      }
+    }
+
+    WHEN("constucted with non-empty initializer list"){
+      THEN("it throws invalid_argument exception"){
+	REQUIRE_THROWS_AS( (tc::array<int,0>{1}) , std::invalid_argument );
+      }
+    }
+  }
+
+}
