5392d7e183508dcc67a61f7308e119a92ea51c89
Reductions <slavi.boyanov@gmail.com>
2016-06-21 06:16:28 +0300

    Implementation for reverse_iterator added

--- source/reverse_iterator_base/reverse_iterator_base_code.h
+++ source/reverse_iterator_base/reverse_iterator_base_code.h
@@ -1,4 +1,50 @@
 #ifndef _REVERSE_ITERATOR_BASE_CODE_H_
 #define _REVERSE_ITERATOR_BASE_CODE_H_
 
+template <class T>
+reverse_iterator_base<T>::reverse_iterator_base(const T& iter):
+  _iter(iter){};
+
+template <class T>
+reverse_iterator_base<T>::operator T() const{
+  return _iter;
+}
+
+template <class T>
+reverse_iterator_base<T> reverse_iterator_base<T>::operator++(){
+  --_iter;
+  return *this;
+}
+
+template <class T>
+reverse_iterator_base<T> reverse_iterator_base<T>::operator++(int){
+  auto copy = *this;
+  --_iter;
+  return copy;
+}
+
+template <class T>
+reverse_iterator_base<T> reverse_iterator_base<T>::operator--(){
+  ++_iter;
+  return *this;
+}
+
+template <class T>
+reverse_iterator_base<T> reverse_iterator_base<T>::operator--(int){
+  auto copy = *this;
+  ++_iter;
+  return copy;
+}
+template <class T>
+bool reverse_iterator_base<T>::
+operator== (const reverse_iterator_base<T>& other) const{
+  return _iter == other._iter;
+}
+
+template <class T>
+bool reverse_iterator_base<T>::
+operator!= (const reverse_iterator_base<T>& other) const{
+  return _iter != other._iter;
+}
+
 #endif//_REVERSE_ITERATOR_BASE_CODE_H_
